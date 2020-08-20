73582e92722bb758e961d247c51dcb1ea0ded246
Abdullahi Abass <36056652+space-w-alker@users.noreply.github.com>
2020-08-20 20:05:04 +0100

    no css output

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './tailwind.output.css';
 
 const App = () => (
-  <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-sm">
+  <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-lg">
     <div className="ml-6 pt-1">
       <h1 className="text-2xl text-blue-700 leading-tight">
         Tailwind and Create React App
